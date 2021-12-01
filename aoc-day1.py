0d92fc96f9d674963485763becf17cbb90551e66
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-01 12:02:10 +0000

    add run times

--- aoc-day1.py
+++ aoc-day1.py
@@ -1,19 +1,29 @@
+import time
+start_time = time.time()
 with open('input\day1.txt','r') as f:
     depths = [int(depth) for depth in f]
 depths_greater_than_previous = 0
 windowed_depths_greater_than_previous = 0
 
+part1_start_time = time.time()
 for count, depth in enumerate(depths):
     if count > 0 and depth > depths[count - 1]:
         depths_greater_than_previous += 1
 
-print("Single depths greater than previous %s " % depths_greater_than_previous)
+part1_end_time = time.time()
 
+part2_start_time = time.time()
 for count, depth in enumerate(depths):
     if count > 2:
         depths_this_window = sum(depths[count-2:count+1])
         depths_prev_window = sum(depths[count-3:count])
         if depths_this_window > depths_prev_window:
             windowed_depths_greater_than_previous+=1
+part2_end_time = time.time()
 
+end_time = time.time()
+print("Single depths greater than previous %s " % depths_greater_than_previous)
 print("Windowed depths greater than previous %s" % windowed_depths_greater_than_previous)
+print(f'Part1 Run time is {part1_end_time - part1_start_time}')
+print(f'Part2 Run time is {part2_end_time - part2_start_time}')
+print(f'Total Run time is {end_time - start_time}')
