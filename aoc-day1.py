39c9c5206adb4b15699073aeb935df05756d2d41
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-01 11:45:09 +0000

    Part two solution

--- aoc-day1.py
+++ aoc-day1.py
@@ -1,13 +1,19 @@
-f = open('input\day1.txt','r')
-depths = f.readlines()
+with open('input\day1.txt','r') as f:
+    depths = [int(depth) for depth in f]
 depths_greater_than_previous = 0
+windowed_depths_greater_than_previous = 0
 
 for count, depth in enumerate(depths):
-    if count > 0 and int(depth) > int(depths[count - 1]):
+    if count > 0 and depth > depths[count - 1]:
         depths_greater_than_previous += 1
 
-print(depths_greater_than_previous)
+print("Single depths greater than previous %s " % depths_greater_than_previous)
 
+for count, depth in enumerate(depths):
+    if count > 2:
+        depths_this_window = sum(depths[count-2:count+1])
+        depths_prev_window = sum(depths[count-3:count])
+        if depths_this_window > depths_prev_window:
+            windowed_depths_greater_than_previous+=1
 
-
-
+print("Windowed depths greater than previous %s" % windowed_depths_greater_than_previous)
