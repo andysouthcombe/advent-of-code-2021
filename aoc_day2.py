6f01c5468aef13d704d1af58bebde97629beb0fe
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-02 12:14:52 +0000

    day 2 sub can move

--- aoc_day2.py
+++ aoc_day2.py
@@ -2,13 +2,14 @@ class Submarine:
     def __init__(self):
         self.depth = 0
         self.horizontal = 0
-        self.mulitpliers = {'foward':1, 'up': -1, 'down':1}
+        self.mulitpliers = {'forward':1, 'up': -1, 'down':1}
 
-    def move(self, direction,distance):
+    def move(self, directions):
+        (direction, distance) = directions
         if direction == 'forward':
-            self.horizontal += (distance * self.mulitpliers['direction'])
+            self.horizontal += (distance * self.mulitpliers[direction])
         else:
-            self.depth += (distance * self.mulitpliers['direction'])
+            self.depth += (distance * self.mulitpliers[direction])
         return f'new position is H: {self.horizontal}, D: {self.depth}'
 
 def read_input(filename):
--- aoc_day2_test.py
+++ aoc_day2_test.py
@@ -1,5 +1,6 @@
 import pytest
 from aoc_day2 import read_input
+from aoc_day2 import Submarine
 
 def test_read_input_content():
     directions = read_input('input\\day2_test.txt')
@@ -11,3 +12,24 @@ def test_read_input_types():
     (direction, distance) = directions[1]
     assert isinstance(direction, str)
     assert isinstance(distance, int)
+
+def test_sub_can_move_forward():
+    sub = Submarine()
+    directions = ('forward', 1)
+    sub.move(directions)
+    assert sub.horizontal == 1
+    assert sub.depth == 0
+
+def test_sub_can_move_up():
+    sub = Submarine()
+    directions = ('up', 1)
+    sub.move(directions)
+    assert sub.horizontal == 0
+    assert sub.depth == -1
+
+def test_sub_can_move_down():
+    sub = Submarine()
+    directions = ('down', 2)
+    sub.move(directions)
+    assert sub.horizontal == 0
+    assert sub.depth == 2
