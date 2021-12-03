fc35903188fed1f2fe530bd9b171b6795009ffe0
Andy Southcombe <andy.southcombe@gmail.com>
2021-12-03 10:27:09 +0000

    Calculates epsilon and gamma rates

--- aoc_day3.py
+++ aoc_day3.py
@@ -1,5 +1,9 @@
 from collections import Counter
 
+def read_input(filename):
+    with open(filename,'r') as f:
+        return f.read().splitlines()
+
 def get_column_counts(input_list, column_num):
     column_vals = [row[column_num] for row in input_list]
     return Counter(column_vals)
@@ -8,6 +12,20 @@ def most_common_bit(input_list, column_num):
     counts = get_column_counts(input_list, column_num)
     return counts.most_common()[0][0]
 
-def read_input(filename):
-    with open(filename,'r') as f:
-        return f.readlines()
+def least_common_bit(input_list, column_num):
+    counts = get_column_counts(input_list, column_num)
+    return counts.most_common()[-1][0]
+
+def get_gamma_rate(input_list):
+    num_cols = len(input_list[0])
+    most_popular_bits = ''
+    for c in range(num_cols):
+        most_popular_bits += most_common_bit(input_list, c)
+    return most_popular_bits
+
+def get_epsilon_rate(input_list):
+    num_cols = len(input_list[0])
+    least_popular_bits = ''
+    for c in range(num_cols):
+        least_popular_bits += least_common_bit(input_list, c)
+    return least_popular_bits
--- aoc_day3_part1_test.py
+++ aoc_day3_part1_test.py
@@ -1,13 +1,29 @@
 import pytest
 
-from aoc_day3 import most_common_bit,read_input
+from aoc_day3 import get_epsilon_rate, get_gamma_rate, least_common_bit, most_common_bit, read_input
 
-def test_returns_correct_count_for_first_test_column():
-    input_bits = read_input('input\\day3_test.txt')
+input_bits = read_input('input\\day3_test.txt')
+
+def test_returns_correct_common_bit_for_first_test_column():
     output = most_common_bit(input_bits,0)
     assert output == '1'
 
-def test_returns_correct_count_for_second_test_column():
-    input_bits = read_input('input\\day3_test.txt')
+def test_returns_correct_common_bit_for_second_test_column():
     output = most_common_bit(input_bits,1)
     assert output == '0'
+
+def test_get_gamma_rate_works_for_sample():
+    output = get_gamma_rate(input_bits)
+    assert output == '10110'
+
+def test_returns_correct_least_common_bit_for_first_test_column():
+    output = least_common_bit(input_bits,0)
+    assert output == '0'
+
+def test_returns_correct_least_common_bit_for_second_test_column():
+    output = least_common_bit(input_bits,1)
+    assert output == '1'
+
+def test_get_epsilon_rate_works_for_sample():
+    output = get_epsilon_rate(input_bits)
+    assert output == '01001'
