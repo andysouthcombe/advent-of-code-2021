ca2ae9fe60e1db561831dc1fb51d4131bd8cd2ac
Andy Southcombe <andy.southcombe@gmail.com>
2021-12-04 13:26:36 +0000

    reads card rows ok

--- aoc_day4.py
+++ aoc_day4.py
@@ -1,7 +1,33 @@
+class Card:
+    def __init__(self):
+        self.rows = []
+    
+
 def read_file(filename):
     with open(filename,'r') as f:
         return f.read().splitlines()
 
 def read_numbers(filename):
     raw_numbers = read_file(filename)
-    return raw_numbers[0].split(',')
+    return [int(num) for num in raw_numbers[0].split(',')]
+
+def read_cards(filename):
+    raw_cards = read_file(filename)
+    cards = []
+    card = Card()
+    row_num_for_this_card = 0
+    for line in raw_cards:
+        if len(line) > 0: 
+            card.rows.append([int(number) for number in line.split()])
+            if  row_num_for_this_card == 4:
+                cards.append(card)
+                card = Card()
+                row_num_for_this_card = 0
+            else:
+                row_num_for_this_card +=1
+        else:
+            #skip empty line
+            continue
+
+    return cards
+
--- aoc_day4_test.py
+++ aoc_day4_test.py
@@ -1,9 +1,15 @@
 import pytest
 
-from aoc_day4 import read_numbers
+from aoc_day4 import read_cards, read_numbers, Card
 
 numbers = read_numbers('input\\day_4_test_numbers.txt')
+cards = read_cards('input\\day_4_test_cards.txt')
 
 def test_numbers_read_in_ok():
-    assert numbers[0] == '7'
+    assert numbers[0] == 7
     assert len(numbers) == 27
+
+def test_card_rows_read_in_ok():
+    assert cards[0].rows[0] == [22,13,17,11,0]
+    assert cards[2].rows[4] == [2,0,12,3,7]
+
