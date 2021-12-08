42165441a57012d159b33b74422eac82f53a053e
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 08:56:25 +0000

    timer resets to 6 from 0]

--- aoc_day6.py
+++ aoc_day6.py
@@ -3,5 +3,11 @@ def read_file(filename):
         line = f.read().rstrip()
         return [int(num) for num in line.split(',')]
 
+def decrement_timer(fish_timer):
+    if fish_timer -1 < 0:
+        return 6
+    else:
+        return fish_timer -1
+
 def day_pass(fishes):
-    return [fish_timer - 1 for fish_timer in fishes]
+    return [decrement_timer(fish_timer) for fish_timer in fishes]
--- aoc_day6_test.py
+++ aoc_day6_test.py
@@ -8,5 +8,10 @@ test_fishes = read_file('input\\day6_test.txt')
 def test_file_read_ok():
     assert len(test_fishes) == 5
 
-def timer_decrements_number_greater_than_1():
+def test_timer_decrements_number_greater_than_1():
     assert day_pass(test_fishes)[0] == 2
+
+def test_timer_decrements_0_to_6():
+    day_1 = day_pass(test_fishes)
+    day_2 = day_pass(day_1)
+    assert day_2[3] == 6
