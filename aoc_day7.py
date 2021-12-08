4b2e43678466eb35cbcc5cedf61ef44518c33560
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 16:14:22 +0000

    remove empty space

--- aoc_day7.py
+++ aoc_day7.py
@@ -19,10 +19,6 @@ def get_move_cost_part_2(current_position, target_position, costs_dict):
     distance = abs(target_position - current_position)
     return costs_dict[distance]
 
-
-
-
-
 def get_total_move_cost_to_position(sub_positions, target_position,get_move_cost_part_1_or_2,costs_dict=None):
     return sum([get_move_cost_part_1_or_2(sub_position,target_position,costs_dict) for sub_position in sub_positions])
 
