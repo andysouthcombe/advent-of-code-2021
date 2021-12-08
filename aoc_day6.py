4bd95284555a922ea6fdb8665b4d02eb97e7da44
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 08:50:36 +0000

    decrements timer

--- aoc_day6.py
+++ aoc_day6.py
@@ -2,3 +2,6 @@ def read_file(filename):
     with open(filename,'r') as f:
         line = f.read().rstrip()
         return [int(num) for num in line.split(',')]
+
+def day_pass(fishes):
+    return [fish_timer - 1 for fish_timer in fishes]
--- aoc_day6_test.py
+++ aoc_day6_test.py
@@ -1,9 +1,12 @@
 from os import read
 import pytest
 
-from aoc_day6 import read_file
+from aoc_day6 import day_pass, read_file
 
-test_numbers = read_file('input\\day6_test.txt')
+test_fishes = read_file('input\\day6_test.txt')
 
 def test_file_read_ok():
-    assert len(test_numbers) == 5
+    assert len(test_fishes) == 5
+
+def timer_decrements_number_greater_than_1():
+    assert day_pass(test_fishes)[0] == 2
