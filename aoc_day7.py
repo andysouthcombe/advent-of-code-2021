a036e9edec3b82d1afd2b5d1d14aa054addc2c14
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 16:06:10 +0000

    refactor part 2 to be instant

--- aoc_day7.py
+++ aoc_day7.py
@@ -3,20 +3,33 @@ from aoc_utils import read_one_line_int_file
 def get_crab_sub_position_range(sub_positions):
     return min(sub_positions), max(sub_positions)
 
-def get_move_cost(current_position,target_position):
+def get_move_cost(current_position,target_position,costs_dict=None):
     return abs(target_position - current_position)
 
-def get_move_cost_part_2(current_position,target_position):
+def build_cost_dict(minimum_position, maximum_position):
+    costs = {}
+    cost = 0
+    for dist in range(0, maximum_position - minimum_position + 1):
+        cost += dist
+        costs[dist] = cost
+
+    return costs
+
+def get_move_cost_part_2(current_position, target_position, costs_dict):
     distance = abs(target_position - current_position)
-    return sum([step for step in range(0, distance+1)])
+    return costs_dict[distance]
 
 
-def get_total_move_cost_to_position(sub_positions, target_position,get_move_cost_part_1_or_2):
-    return sum([get_move_cost_part_1_or_2(sub_position,target_position) for sub_position in sub_positions])
+
+
+
+def get_total_move_cost_to_position(sub_positions, target_position,get_move_cost_part_1_or_2,costs_dict=None):
+    return sum([get_move_cost_part_1_or_2(sub_position,target_position,costs_dict) for sub_position in sub_positions])
 
 def get_cheapest_common_position(sub_positions, get_move_cost_part_1_or_2):
     min_position, max_position = get_crab_sub_position_range(sub_positions)
-    return min([(target_position, get_total_move_cost_to_position(sub_positions,target_position,get_move_cost_part_1_or_2)) for target_position in range(min_position, max_position + 1)], key = lambda cost:cost[1])
+    costs_dict = build_cost_dict(min_position, max_position)
+    return min([(target_position, get_total_move_cost_to_position(sub_positions,target_position,get_move_cost_part_1_or_2,costs_dict)) for target_position in range(min_position, max_position + 1)], key = lambda cost:cost[1])
     
 if __name__ == '__main__':
     sub_positions = read_one_line_int_file('input\\day7.txt')
--- aoc_day7_test.py
+++ aoc_day7_test.py
@@ -2,12 +2,13 @@ from os import read
 import pytest
 
 from aoc_utils import read_one_line_int_file
-from aoc_day7 import get_crab_sub_position_range,get_move_cost,get_total_move_cost_to_position,get_cheapest_common_position,get_move_cost_part_2
+from aoc_day7 import get_crab_sub_position_range,get_move_cost,get_total_move_cost_to_position,get_cheapest_common_position,get_move_cost_part_2,build_cost_dict
 
 test_crab_subs = read_one_line_int_file('input\\day7_test.txt')
+min_position, max_position = get_crab_sub_position_range(test_crab_subs)
+costs_dict = build_cost_dict(min_position, max_position)
 
 def test_get_crab_sub_position_range():
-    min_position, max_position = get_crab_sub_position_range(test_crab_subs)
     assert (min_position, max_position) == (0, 16)
 
 def test_get_move_cost_positive_move():
@@ -23,7 +24,7 @@ def test_get_cheapest_common_position():
     assert get_cheapest_common_position(test_crab_subs,get_move_cost) == (2, 37)
 
 def test_get_move_cost_part_2():
-    assert get_move_cost_part_2(0,1) == 1
-    assert get_move_cost_part_2(0,2) == 3
-    assert get_move_cost_part_2(2,5) == 6
-    assert get_move_cost_part_2(14,5) == 45
+    assert get_move_cost_part_2(0,1, costs_dict) == 1
+    assert get_move_cost_part_2(0,2, costs_dict) == 3
+    assert get_move_cost_part_2(2,5, costs_dict) == 6
+    assert get_move_cost_part_2(14,5, costs_dict) == 45
