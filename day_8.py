ca7ee3b518fd3ba0bdae8763bc532ed258a21eaf
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 17:52:08 +0000

    matches signals to the easy numbers

---
+++ day_8.py
@@ -0,0 +1,38 @@
+signal_count_to_digit_dict = {
+    2: 1,
+    5: [2,3,5],
+    4: 4,
+    6: [0, 6, 9],
+    3: 7,
+    7: 8
+}
+
+def read_and_split_file(filename):
+    with open(filename,'r') as f:
+        lines = f.readlines()
+        signals = []
+        outputs = []
+        for line in lines:
+            signal_and_output = line.rstrip().split(' | ')
+            signals.append(signal_and_output[0].split())
+            outputs.append(signal_and_output[1].split())
+        return signals, outputs
+
+def count_digits_in_output_with_distinct_signal_count(output_list):
+    counter = 0
+    for output_line in output_list:
+        for output in output_line:
+            if isinstance(signal_count_to_digit_dict[len(output)], int):
+                counter += 1
+    return counter
+
+def get_signals_with_distinct_counts(signals):
+    return [signal for signal in signals if len(signal) in [2,4,3,7]]
+
+def identify_signals_for_easy_numbers(distinct_signals):
+    return [(signal, signal_count_to_digit_dict[len(signal)]) for signal in distinct_signals]
+        
+
+if __name__ == '__main__':
+    signals, outputs = read_and_split_file('input\\day8.txt')
+    print(count_digits_in_output_with_distinct_signal_count(outputs))
---
+++ day_8_test.py
@@ -0,0 +1,19 @@
+import pytest
+
+from day_8 import count_digits_in_output_with_distinct_signal_count, get_signals_with_distinct_counts, identify_signals_for_easy_numbers, read_and_split_file
+
+test_signals, test_outputs = read_and_split_file('input\\day8_test.txt')
+
+def test_file_read_ok():
+    assert test_signals[0][2] == 'cbdgef'
+    assert test_signals[1][3] == 'gc'
+    assert test_outputs[2][3] == 'cbg'
+
+def test_count_distinct_digits_in_test_output():
+    assert count_digits_in_output_with_distinct_signal_count(test_outputs) == 26
+
+def test_get_signals_with_distinct_counts_returns_right_signals():
+    assert get_signals_with_distinct_counts(test_signals[0])== ['be', 'cfbegad', 'cgeb', 'edb']
+
+def test_identify_signals_for_easy_numbers():
+    assert identify_signals_for_easy_numbers(['be', 'cfbegad', 'cgeb', 'edb']) == [('be', 1),('cfbegad', 8), ('cgeb', 4),('edb',7)]
