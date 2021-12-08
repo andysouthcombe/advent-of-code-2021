9636370bb7ffe2791026ca337f0f9850532a6ab8
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 09:59:04 +0000

    fish per age decrements

--- aoc_day6.py
+++ aoc_day6.py
@@ -9,28 +9,26 @@ def get_number_fish_per_age(fishes):
         fish_per_age.append(fishes.count(age))
     return fish_per_age
 
-def decrement_timer(fish_timer):
-    if fish_timer -1 < 0:
-        return 6
-    else:
-        return fish_timer -1
+def day_pass(fish_per_age):
+    fish_day_later = []
+    for index in range(0,len(fish_per_age)):
+        if index < 8:
+            fish_day_later.append(fish_per_age[index+1])
+    return fish_day_later
 
-def day_pass(fishes):
-    fish_about_to_breed = [fish_timer for fish_timer in fishes if fish_timer == 0]
-    decremented_timers =[decrement_timer(fish_timer) for fish_timer in fishes]
-    return decremented_timers + [8 for fish in fish_about_to_breed]
+        
 
-def pass_days(fishes,number_of_days):
-    output_fishes = fishes
-    day = 1
-    while day <= number_of_days:
-        output_fishes = day_pass(output_fishes)
-        day += 1
-    return output_fishes
+# def pass_days(fishes,number_of_days):
+#     output_fishes = fishes
+#     day = 1
+#     while day <= number_of_days:
+#         output_fishes = day_pass(output_fishes)
+#         day += 1
+#     return output_fishes
 
-if __name__ == '__main__':
-    fishes = read_file('input\\day6.txt')
-    resulting_fish = pass_days(fishes, 80)
-    print(f'After 80 days we have {len(resulting_fish)} fish')
-    #resulting_fish = pass_days(fishes, 256)
-    print(f'After 256 days we have {len(resulting_fish)} fish')
+# if __name__ == '__main__':
+    # fishes = read_file('input\\day6.txt')
+    # resulting_fish = pass_days(fishes, 80)
+    # print(f'After 80 days we have {len(resulting_fish)} fish')
+    # #resulting_fish = pass_days(fishes, 256)
+    # print(f'After 256 days we have {len(resulting_fish)} fish')
--- aoc_day6_test.py
+++ aoc_day6_test.py
@@ -1,7 +1,7 @@
 from os import read
 import pytest
 
-from aoc_day6 import day_pass, get_number_fish_per_age, pass_days, read_file
+from aoc_day6 import day_pass, get_number_fish_per_age, read_file
 
 test_fishes = read_file('input\\day6_test.txt')
 fish_per_age = get_number_fish_per_age(test_fishes)
@@ -13,21 +13,21 @@ def test_fish_per_age_totals_test_input():
     assert len(fish_per_age) == 9
     assert fish_per_age == [0,1,1,2,1,0,0,0,0]
 
-def test_timer_decrements_number_greater_than_1():
-    assert day_pass(test_fishes)[0] == 2
+def test_day_pass_moves_fish_ages_down():
+    assert day_pass(fish_per_age)[0] == 1
 
-def test_timer_decrements_0_to_6():
-    day_1 = day_pass(test_fishes)
-    day_2 = day_pass(day_1)
-    assert day_2[3] == 6
+# def test_timer_decrements_0_to_6():
+#     day_1 = day_pass(test_fishes)
+#     day_2 = day_pass(day_1)
+#     assert day_2[3] == 6
 
-def test_extra_fish_when_hit_zero():
-    day_1 = day_pass(test_fishes)
-    day_2 = day_pass(day_1)
-    assert len(day_2) == len(test_fishes)+1
-    assert day_2[-1] == 8
+# def test_extra_fish_when_hit_zero():
+#     day_1 = day_pass(test_fishes)
+#     day_2 = day_pass(day_1)
+#     assert len(day_2) == len(test_fishes)+1
+#     assert day_2[-1] == 8
 
-def test_pass_days_gives_right_number_of_fish_for_test_case():
-    output_fish = pass_days(test_fishes, 18)
-    print(output_fish)
-    assert len(output_fish) == 26
+# def test_pass_days_gives_right_number_of_fish_for_test_case():
+#     output_fish = pass_days(test_fishes, 18)
+#     print(output_fish)
+#     assert len(output_fish) == 26
