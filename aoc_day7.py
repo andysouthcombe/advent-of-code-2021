1c362a0ad7334be0ceb28cdb6c6a0e0435a95bc9
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 11:14:52 +0000

    Calculates move cost

--- aoc_day7.py
+++ aoc_day7.py
@@ -2,3 +2,6 @@ from aoc_utils import read_one_line_int_file
 
 def get_crab_sub_position_range(sub_positions):
     return min(sub_positions), max(sub_positions)
+
+def get_move_cost(current_position,target_position):
+    return abs(target_position - current_position)
--- aoc_day7_test.py
+++ aoc_day7_test.py
@@ -2,7 +2,7 @@ from os import read
 import pytest
 
 from aoc_utils import read_one_line_int_file
-from aoc_day7 import get_crab_sub_position_range
+from aoc_day7 import get_crab_sub_position_range,get_move_cost
 
 test_crab_subs = read_one_line_int_file('input\\day7_test.txt')
 
@@ -10,3 +10,8 @@ def test_get_crab_sub_position_range():
     min_position, max_position = get_crab_sub_position_range(test_crab_subs)
     assert (min_position, max_position) == (0, 16)
 
+def test_get_move_cost_positive_move():
+    assert get_move_cost(0,3) == 3
+
+def test_get_move_cost_negative_move():
+    assert get_move_cost(3,1) == 2
