fa63540ef88433ceb3550be567495a1b17ed4b87
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 09:14:20 +0000

    passes part 1 test

--- aoc_day6.py
+++ aoc_day6.py
@@ -10,4 +10,14 @@ def decrement_timer(fish_timer):
         return fish_timer -1
 
 def day_pass(fishes):
-    return [decrement_timer(fish_timer) for fish_timer in fishes]
+    fish_about_to_breed = [fish_timer for fish_timer in fishes if fish_timer == 0]
+    decremented_timers =[decrement_timer(fish_timer) for fish_timer in fishes]
+    return decremented_timers + [8 for fish in fish_about_to_breed]
+
+def pass_days(fishes,number_of_days):
+    output_fishes = fishes
+    day = 1
+    while day <= number_of_days:
+        output_fishes = day_pass(output_fishes)
+        day += 1
+    return output_fishes
--- aoc_day6_test.py
+++ aoc_day6_test.py
@@ -1,7 +1,7 @@
 from os import read
 import pytest
 
-from aoc_day6 import day_pass, read_file
+from aoc_day6 import day_pass, pass_days, read_file
 
 test_fishes = read_file('input\\day6_test.txt')
 
@@ -15,3 +15,14 @@ def test_timer_decrements_0_to_6():
     day_1 = day_pass(test_fishes)
     day_2 = day_pass(day_1)
     assert day_2[3] == 6
+
+def test_extra_fish_when_hit_zero():
+    day_1 = day_pass(test_fishes)
+    day_2 = day_pass(day_1)
+    assert len(day_2) == len(test_fishes)+1
+    assert day_2[-1] == 8
+
+def test_pass_days_gives_right_number_of_fish_for_test_case():
+    output_fish = pass_days(test_fishes, 18)
+    print(output_fish)
+    assert len(output_fish) == 26
