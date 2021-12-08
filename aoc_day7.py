89632ad50b9ad9a4610ceaedc492c454b91ff4f9
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-08 11:50:43 +0000

    solves part 2 (very slowly)

--- aoc_day7.py
+++ aoc_day7.py
@@ -6,10 +6,19 @@ def get_crab_sub_position_range(sub_positions):
 def get_move_cost(current_position,target_position):
     return abs(target_position - current_position)
 
-def get_total_move_cost_to_position(sub_positions, target_position):
-    return sum([get_move_cost(sub_position,target_position) for sub_position in sub_positions])
+def get_move_cost_part_2(current_position,target_position):
+    distance = abs(target_position - current_position)
+    return sum([step for step in range(0, distance+1)])
 
-def get_cheapest_common_position(sub_positions):
+
+def get_total_move_cost_to_position(sub_positions, target_position,get_move_cost_part_1_or_2):
+    return sum([get_move_cost_part_1_or_2(sub_position,target_position) for sub_position in sub_positions])
+
+def get_cheapest_common_position(sub_positions, get_move_cost_part_1_or_2):
     min_position, max_position = get_crab_sub_position_range(sub_positions)
-    return min([(target_position, get_total_move_cost_to_position(sub_positions,target_position)) for target_position in range(min_position, max_position + 1)], key = lambda cost:cost[1])[0]
+    return min([(target_position, get_total_move_cost_to_position(sub_positions,target_position,get_move_cost_part_1_or_2)) for target_position in range(min_position, max_position + 1)], key = lambda cost:cost[1])
     
+if __name__ == '__main__':
+    sub_positions = read_one_line_int_file('input\\day7.txt')
+    print(f'cheapest position to move to for part 1 is {get_cheapest_common_position(sub_positions, get_move_cost)}')
+    print(f'cheapest position to move to for part 2 is {get_cheapest_common_position(sub_positions, get_move_cost_part_2)}')
--- aoc_day7_test.py
+++ aoc_day7_test.py
@@ -2,7 +2,7 @@ from os import read
 import pytest
 
 from aoc_utils import read_one_line_int_file
-from aoc_day7 import get_crab_sub_position_range,get_move_cost,get_total_move_cost_to_position,get_cheapest_common_position
+from aoc_day7 import get_crab_sub_position_range,get_move_cost,get_total_move_cost_to_position,get_cheapest_common_position,get_move_cost_part_2
 
 test_crab_subs = read_one_line_int_file('input\\day7_test.txt')
 
@@ -17,7 +17,13 @@ def test_get_move_cost_negative_move():
     assert get_move_cost(3,1) == 2
 
 def test_get_total_move_cost_to_position():
-    assert get_total_move_cost_to_position(test_crab_subs,1) == 41
+    assert get_total_move_cost_to_position(test_crab_subs,1,get_move_cost) == 41
 
 def test_get_cheapest_common_position():
-    assert get_cheapest_common_position(test_crab_subs) == 2
+    assert get_cheapest_common_position(test_crab_subs,get_move_cost) == (2, 37)
+
+def test_get_move_cost_part_2():
+    assert get_move_cost_part_2(0,1) == 1
+    assert get_move_cost_part_2(0,2) == 3
+    assert get_move_cost_part_2(2,5) == 6
+    assert get_move_cost_part_2(14,5) == 45
