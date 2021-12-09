fa2dc11c4a1d03901412ccc12653d4d4a1d6a357
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-09 18:20:37 +0000

    finds neighbours for middle squares

--- aoc_day9.py
+++ aoc_day9.py
@@ -1,4 +1,10 @@
 def read_numbers(filename):
     with open(filename,'r') as f:
         return [line.rstrip() for line in f.readlines()]
-        
+
+def get_neighbour_square_values(test_numbers, x, y):
+    above = test_numbers[y-1][x]
+    right = test_numbers[y][x+1]
+    left = test_numbers[y][x-1]
+    below = test_numbers[y+1][x]
+    return [above, right,below,left]
