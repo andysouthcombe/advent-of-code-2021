519aad477abb7f8f66ae1db8b7efefcf95281819
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-15 09:43:09 +0000

    detects simple invalid input

--- aoc_day10.py
+++ aoc_day10.py
@@ -5,7 +5,7 @@ open_to_closing_chars = {
                     '{': '}'
                 }
 
-closing_to_open_chars = {value:key for value, key in open_to_closing_chars.items()}
+closing_to_open_chars = {value:key for key, value in open_to_closing_chars.items()}
 
 def get_invalid_closing_character(input):
     open_chars = []
@@ -13,7 +13,9 @@ def get_invalid_closing_character(input):
     for char in input:
         if char in open_to_closing_chars:
             open_chars.append(char)
-        if char in closing_to_open_chars:
+        elif closing_to_open_chars[char] not in open_chars:
+            return char
+        else:
             close_chars.append(char)
     if (len(open_chars) == len(close_chars)):
         return None
--- aoc_day10_test.py
+++ aoc_day10_test.py
@@ -8,4 +8,7 @@ test_input_invalid_1 = '(]'
 test_input_invalid_2 = '{()()()>'
 
 def test_simple_valid_input_returns_none():
-    assert get_invalid_closing_character(test_input_invalid_1) == None
+    assert get_invalid_closing_character(test_input_valid_1) == None
+
+def test_simple_invalid_input_returns_square_bracket():
+    assert get_invalid_closing_character(test_input_invalid_1) == ']'
