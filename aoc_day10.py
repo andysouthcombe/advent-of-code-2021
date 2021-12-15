d2265481d4851fa7e85d2a8d78d0938fafa5bcf7
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-15 09:50:05 +0000

    works for more complex invalid input

--- aoc_day10.py
+++ aoc_day10.py
@@ -9,13 +9,12 @@ closing_to_open_chars = {value:key for key, value in open_to_closing_chars.items
 
 def get_invalid_closing_character(input):
     open_chars = []
-    close_chars = []
     for char in input:
         if char in open_to_closing_chars:
             open_chars.append(char)
-        elif closing_to_open_chars[char] not in open_chars:
+            continue
+        try:
+            open_chars.remove(closing_to_open_chars[char])
+        except(ValueError):
             return char
-        else:
-            close_chars.append(char)
-    if (len(open_chars) == len(close_chars)):
-        return None
+    return None
