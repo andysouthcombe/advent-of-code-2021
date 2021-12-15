2bcb81ce5c4738570e587134db67170f6799a141
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-15 10:52:52 +0000

    pass part 1

--- aoc_day10.py
+++ aoc_day10.py
@@ -1,20 +1,37 @@
+from os import close
+from aoc_utils import read_file_of_strings
+
 open_to_closing_chars = {
-                    '(': ')',
-                    '<': '>',
-                    '[': ']',
-                    '{': '}'
-                }
+                            '(': ')',
+                            '<': '>',
+                            '[': ']',
+                            '{': '}'
+                        }
 
 closing_to_open_chars = {value:key for key, value in open_to_closing_chars.items()}
+
+invalid_closing_char_scores = {
+                                ')': 3,
+                                ']': 57,
+                                '}': 1197,
+                                '>': 25137
+                                }
 
 def get_invalid_closing_character(input):
     open_chars = []
     for char in input:
         if char in open_to_closing_chars:
             open_chars.append(char)
-            continue
-        try:
-            open_chars.remove(closing_to_open_chars[char])
-        except(ValueError):
-            return char
+        if char in closing_to_open_chars:
+            if open_chars[-1] == closing_to_open_chars[char]:
+                open_chars.pop(-1)
+            else:
+                return char
     return None
+
+def score_invalid_chars_part_1(inputs):
+    invalid_chars = [get_invalid_closing_character(input) for input in inputs]
+    return sum([invalid_closing_char_scores[invalid_char] for invalid_char in invalid_chars if invalid_char is not None])
+
+if __name__ == '__main__':
+    print(get_invalid_closing_character('{([(<{}[<>[]}>{[]{[(<()>'))
--- aoc_day10_test.py
+++ aoc_day10_test.py
@@ -1,12 +1,20 @@
 import pytest
 
-from aoc_day10 import get_invalid_closing_character
+from aoc_day10 import get_invalid_closing_character, score_invalid_chars_part_1
+from aoc_utils import read_file_of_strings
 
 test_input_valid_1 = '()'
 test_input_valid_2 = '()()<>{()}'
 test_input_invalid_1 = '(]'
 test_input_invalid_2 = '{()()<>()>'
-
+test_input_invalid_3 = '{([(<{}[<>[]}>{[]{[(<()>'
+test_input_part_1 = [
+                        '{([(<{}[<>[]}>{[]{[(<()>',
+                        '[[<[([]))<([[{}[[()]]]',
+                        '[{[{({}]{}}([{[{{{}}([]',
+                        '[<(<(<(<{}))><([]([]()',
+                        '<{([([[(<>()){}]>(<<{{'
+                    ] 
 
 def test_simple_valid_input_returns_none():
     assert get_invalid_closing_character(test_input_valid_1) == None
@@ -19,3 +27,9 @@ def test_complex_valid_input_returns_none():
 
 def test_complex_valid_input_returns_none():
     assert get_invalid_closing_character(test_input_invalid_2) == '>'
+
+def test_very_complex_valid_input_returns_none():
+    assert get_invalid_closing_character(test_input_invalid_3) == '}'
+
+def test_score_invalid_chars_part_1_works_test_input():
+    assert score_invalid_chars_part_1(test_input_part_1) == 26397
