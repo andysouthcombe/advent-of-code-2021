7efc3af752dd8cfafa9e07be7bf862951242336f
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-17 16:42:08 +0000

    change data structure

--- aoc_day11.py
+++ aoc_day11.py
@@ -1,23 +1,11 @@
 from aoc_utils import read_file_of_strings
-from dataclasses import dataclass
 
-@dataclass
-class Octopus:
-    def __init__(self, energy) -> None:
-        self.energy = int(energy)
-    
-    def take_step(self):
-        self.energy += 1
-        if self.energy == 10:
-            self.energy = 0
+class OctopiGrid:
+    def __init__(self, octopi_lines):
+        self.octopi_lines = octopi_lines
 
+    def get_octopus(self, x, y):
+        return int(self.octopi_lines[y][x])
 
 def initialise_octopi_grid(filename):
-    initial_octopi = read_file_of_strings(filename)
-    octopi_grid = []
-    for line in initial_octopi:
-        octopus_line = []
-        for octopus_energy in line:
-            octopus_line.append(Octopus(octopus_energy))
-        octopi_grid.append(octopus_line)
-    return octopi_grid
+    return  OctopiGrid(read_file_of_strings(filename))
--- aoc_day11_test.py
+++ aoc_day11_test.py
@@ -1,23 +1,11 @@
 import pytest
 
-from aoc_utils import read_file_of_strings
-from aoc_day11 import Octopus, initialise_octopi_grid
-test_grid = read_file_of_strings('input\\day11_test.txt')
+from aoc_day11 import initialise_octopi_grid
+test_grid = initialise_octopi_grid('input\\day11_test.txt')
 
 def test_grid_read_ok():
-    assert test_grid[9] == '5283751526'
+    assert test_grid.octopi_lines[9] == '5283751526'
 
-def test_octopi_grid_set_up_ok():
-    octopi_grid = initialise_octopi_grid('input\\day11_test.txt')
-    assert octopi_grid[0][0]  == Octopus(5)
-    assert octopi_grid[9][9]  == Octopus(6)
-
-def test_ocotopus_energy_increases_when_takes_step():
-    test_octopi_1 = Octopus(0)
-    test_octopi_1.take_step()
-    assert test_octopi_1.energy == 1
-
-def test_ocotopus_energy_resets_to_zero_when_flashes():
-    test_octopi_2 = Octopus(9)
-    test_octopi_2.take_step()
-    assert test_octopi_2.energy == 0
+def test_get_octopi():
+    assert test_grid.get_octopus(0,2)  == 5
+    assert test_grid.get_octopus(8,9)  == 2
