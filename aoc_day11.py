fd5615887598b836e84e336c91423a341a61b0e6
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-17 16:58:04 +0000

    flash octopus

--- aoc_day11.py
+++ aoc_day11.py
@@ -6,6 +6,25 @@ class OctopiGrid:
 
     def get_octopus(self, x, y):
         return int(self.octopi_lines[y][x])
+    
+    def increment_octopus(self, x, y):
+        self.octopi_lines[y][x] += 1
+    
+    def flash_octopus(self, x, y):
+        self.octopi_lines[y][x] = 0
+    
+    def take_step(self):
+        for y, octopi_line in enumerate(self.octopi_lines):
+            for x in range(0, len(octopi_line)):
+                self.increment_octopus(x, y)
+                if self.get_octopus(x, y) > 9:
+                    self.flash_octopus(x, y)
+                
+                
 
 def initialise_octopi_grid(filename):
-    return  OctopiGrid(read_file_of_strings(filename))
+    raw_grid = read_file_of_strings(filename)
+    octopi_grid = []
+    for octopi_line in raw_grid:
+        octopi_grid.append([int(octopus) for octopus in octopi_line])
+    return  OctopiGrid(octopi_grid)
--- aoc_day11_test.py
+++ aoc_day11_test.py
@@ -1,11 +1,21 @@
 import pytest
 
-from aoc_day11 import initialise_octopi_grid
+from aoc_day11 import OctopiGrid, initialise_octopi_grid
 test_grid = initialise_octopi_grid('input\\day11_test.txt')
 
 def test_grid_read_ok():
-    assert test_grid.octopi_lines[9] == '5283751526'
+    assert test_grid.octopi_lines[9] == [5, 2, 8, 3, 7, 5, 1, 5, 2, 6]
 
 def test_get_octopi():
     assert test_grid.get_octopus(0,2)  == 5
     assert test_grid.get_octopus(8,9)  == 2
+
+def test_take_step_increases_energy():
+    new_grid = test_grid
+    new_grid.take_step()
+    assert test_grid.get_octopus(8,9)  == 3
+
+def test_take_step_flashes_at_9():
+    single_octopus_grid = OctopiGrid([[9]])
+    single_octopus_grid.take_step()
+    assert single_octopus_grid.get_octopus(0, 0)  == 0
