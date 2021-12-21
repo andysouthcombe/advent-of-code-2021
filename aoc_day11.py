8141e345904a5ce317c179016eacb8962053576a
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-21 09:40:02 +0000

    BFS approach

--- aoc_day11.py
+++ aoc_day11.py
@@ -15,27 +15,31 @@ class OctopiGrid:
     def flash_octopus(self, x, y):
         self.octopi_lines[y][x] = 0
     
-    def check_if_neighbour_flashed(self, x, y, flashed_octopi):
-        for flashed_x, flashed_y in flashed_octopi:
-            if abs(x - flashed_x) <=1 and abs(y - flashed_y) <= 1:
-                return True
+    def check_if_neighbour_flashed(self, x, y, flashed_x, flashed_y):
+        if abs(x - flashed_x) <=1 and abs(y - flashed_y) <= 1:
+            return True
         return False
     
     def take_step(self):
         flashed_octopi = []
+        flashing_queue = []
         for y, octopi_line in enumerate(self.octopi_lines):
             for x in range(0, len(octopi_line)):
                 self.increment_octopus(x, y)
                 if self.get_octopus(x, y) > 9:
                     self.flash_octopus(x, y)
                     flashed_octopi.append((x, y))
-        for y, octopi_line in enumerate(self.octopi_lines):
-            for x in range(0, len(octopi_line)):
-                if self.check_if_neighbour_flashed(x, y, flashed_octopi) and (x,y) not in flashed_octopi:
-                    self.increment_octopus(x, y)
-                    if self.get_octopus(x,y) > 9:
-                        self.flash_octopus(x, y)
-                        flashed_octopi.append((x, y))
+                    flashing_queue.append((x, y))
+        while flashing_queue:
+            flashed_x, flashed_y = flashing_queue.pop()
+            for y, octopi_line in enumerate(self.octopi_lines):
+                for x in range(0, len(octopi_line)):
+                    if self.check_if_neighbour_flashed(x, y, flashed_x,flashed_y) and (x,y) not in flashed_octopi:
+                        self.increment_octopus(x, y)
+                        if self.get_octopus(x,y) > 9:
+                            self.flash_octopus(x, y)
+                            flashed_octopi.append((x, y))
+                            flashing_queue.append((x,y))
         
         
                 
