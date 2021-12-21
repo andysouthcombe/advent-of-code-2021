17aeca5fab3834c1624a47b67bb5b96d5e9c3bde
Andrew Southcombe <andrew.southcombe@JohnLewis.co.uk>
2021-12-21 11:47:57 +0000

    gets all routes from cave

--- aoc_day12.py
+++ aoc_day12.py
@@ -1,7 +1,9 @@
 from aoc_utils import read_file_of_strings
 from dataclasses import dataclass
 
-def parse_routes_list(filename):
+def parse_route_list(filename):
     raw_list = [route.split('-') for route in read_file_of_strings(filename)]
     return [(start, end) for start, end in raw_list] + [(end, start) for start, end in raw_list ]
     
+def get_routes_from_cave(cave, route_list):
+    return [route for route in route_list if cave == route[0]]
--- aoc_day12_test.py
+++ aoc_day12_test.py
@@ -1,12 +1,13 @@
 from os import read
 from aoc_utils import read_file_of_strings
-from aoc_day12 import parse_routes_list
+from aoc_day12 import parse_route_list, get_routes_from_cave
 import pytest
 
-test_map_raw = read_file_of_strings('input\\day12_test.txt')
+route_list = parse_route_list('input\\day12_test.txt')
 
 def test_parses_route_list():
-    route_list = parse_routes_list('input\\day12_test.txt')
-    print(route_list)
     assert len(route_list) == 14
 
+def test_get_routes_from_cave():
+    expected_routes = [('A','c'), ('A','b'), ('A', 'end'), ('A','start')]
+    assert expected_routes == get_routes_from_cave('A',route_list)
